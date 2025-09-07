0f678adc537f1d073ea4683cbd27291776e95e3c
mekane <4631242+mekane84@users.noreply.github.com>
2025-09-07 14:34:45 -0500

    Initial commit

---
+++ src/main/java/com/example/springboot20252/SpringBoot20252Application.java
@@ -0,0 +1,13 @@
+package com.example.springboot20252;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot20252Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot20252Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot20252/SpringBoot20252ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot20252;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot20252ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
